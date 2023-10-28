b1cbd57c5731105a4336908daa1245e05070f76c
Tano <tanoni44@gmail.com>
2023-10-28 17:42:25 -0300

    act

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Log/Login';
+import Singin from './components/Log/Singin';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Cart from './components/cart/Cart';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/singin" element={<Singin/>}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/cart" element={<Cart />}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
