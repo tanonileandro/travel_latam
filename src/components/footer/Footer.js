fc71de25f884416f9950b6e43c9ff6ad8a59824d
Tano <tanoni44@gmail.com>
2023-10-29 10:52:00 -0300

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import './footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/fontawesome-free";
 
 const Footer = () => {
   return (
     <footer className="bg-dark text-white py-5">
       <div className="container text-center">
-        <div className="d-flex justify-content-center gap-4 mb-4">
-          <a href="#" className="text-white text-xl">
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-          <a href="#" className="text-white text-xl">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a href="#" className="text-white text-xl">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a href="#" className="text-white text-xl">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
+        <div className="d-flexrow justify-content-center gap-4 mb-4">
+          <div>
+            <p className="text-sm mb-4 footer-text">Síguenos en redes sociales para obtener las últimas actualizaciones</p>
+          </div>
+          <div className="d-flex justify-content-center gap-4 mb-4">
+            <a href="#" className="text-white"><i className="fab fa-facebook"></i></a>
+            <a href="#" className="text-white"><i className="fab fa-twitter"></i></a>
+            <a href="#" className="text-white"><i className="fab fa-instagram"></i></a>
+            <a href="#" className="text-white"><i className="fab fa-linkedin"></i></a>
+          </div>
         </div>
-        <p className="text-sm mb-4">Síguenos en redes sociales para obtener las últimas actualizaciones</p>
-        <p className="mb-4">(123) 456-7890</p>
-        <p className="mb-4">123 Calle, Ciudad Viaje</p>
-        <p className="text-xs">&copy; 2023 Todos los derechos reservados Travel Latam</p>
+
+        <p className="mb-4 footer-contact">(123) 456-7890</p>
+        <p className="mb-4 footer-address">123 Calle, Ciudad Viaje</p>
+        <p className="text-xs footer-copyright">&copy; 2023 Todos los derechos reservados Travel Latam</p>
       </div>
     </footer>
   );
