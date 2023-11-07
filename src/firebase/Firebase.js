3ac20c9b7a0fbf428c784a638b3d081582fdaf4f
Tanoni Leandro <tanoni44@gmail.com>
2023-11-07 06:14:17 -0300

    Update Firebase.js

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA-GNM5pvnhOsNYSQJbU-5ZTAUPZQk3LNY",
   authDomain: "focus-1acf2.firebaseapp.com",
@@ -14,7 +10,6 @@ const firebaseConfig = {
   appId: "1:134446240729:web:552fe699aa394dff123fc8"
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export default firebaseApp;
